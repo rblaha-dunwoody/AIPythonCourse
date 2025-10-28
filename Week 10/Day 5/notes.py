1027c480fee8bd8ab40b843e1d4c22ba6d332135
rblaha-dunwoody <rblaha@dunwoody.edu>
2025-10-28 15:22:59 -0500

    Completed Week 10 - Day 5

---
+++ Week 10/Day 5/notes.py
@@ -0,0 +1,32 @@
+# Building CNN Architectures with PyTorch
+
+# Building CNN Architectures in PyTorch Using the nn Module
+#   Key Steps
+#       - Define a Model
+#           - Use torch.nn.Module to build CNN layers like convolutional, pooling, and fully connected layers
+# 
+#       - Forward Pass
+#           - Define how input flows through the layers to produce output
+#
+#       - Model Summary
+#           - Inspect the structure and learnable parameters
+
+
+# Training and Evaluating CNN's in PyTorch
+#   Training
+#       - Perform forward and backward passes, calculate loss, and update weights using an optimizer
+#
+#   Evaluation
+#       - Test the model on unseen data and compute metrics like accuracy and loss
+
+
+# Experimenting with CNN Model Design and Tuning Hyperparameters
+#   Experimentation Areas
+#       - Layer Depth
+#           - Add or remove convolutional and pooling layers to observe the impact
+#
+#       - Filter Size
+#           - Experiment with kernel sizes (e.g., 3x3, 5x5)
+#
+#       - Learning Rate
+#           - Adjust the learning rate to improve convergence speed and accuracy
